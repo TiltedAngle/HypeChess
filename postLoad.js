cea85cd8a441e35ae04c53a3e54f7c09362b0ca2
TiltedAngle <83716443+TiltedAngle@users.noreply.github.com>
2022-01-20 06:43:18 +0000

    Create postLoad.js

---
+++ postLoad.js
@@ -0,0 +1,31 @@
+//Top Navigation
+
+const TAB_SELECTOR = document.querySelectorAll('#topnav section>a');
+const TAB_GROUP = document.querySelectorAll('[role="group"]');
+
+// Navbar Animations
+TAB_SELECTOR.forEach((item, index) => {
+    item.addEventListener('mouseenter', e => {
+        if (item.nextSibling == TAB_GROUP[index]) {
+            item.nextSibling.classList.add('topnavHover');
+        }
+    })
+
+    item.addEventListener('mouseleave', e => {
+        if (item.nextSibling == TAB_GROUP[index]) {
+            item.nextSibling.classList.remove('topnavHover');
+        }
+    })
+
+    item.nextSibling.addEventListener('mouseenter', e => {
+        if (item.nextSibling == TAB_GROUP[index]) {
+            item.nextSibling.classList.add('topnavHover');
+        }
+    })
+
+    item.nextSibling.addEventListener('mouseleave', e => {
+        if (item.nextSibling == TAB_GROUP[index]) {
+            item.nextSibling.classList.remove('topnavHover');
+        }
+    })
+})
